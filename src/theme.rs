8b88d608babd164212737210903162513d3c825a
Yared Betemariam <yaredbelaybtm@gmail.com>
2026-09-17 21:42:57 +0300

    feat: scroller color and border color changed

--- src/theme.rs
+++ src/theme.rs
@@ -136,7 +136,7 @@ impl Theme {
         if self.is_dark() {
             Color32::from_rgba_premultiplied(24, 24, 24, 100)
         } else {
-            Color32::from_rgba_premultiplied(24, 24, 24, 65)
+            Color32::from_rgba_premultiplied(32, 32, 32, 65)
         }
     }
 
@@ -300,6 +300,9 @@ pub fn apply_theme(ctx: &egui::Context, theme: &Theme) {
     visuals.widgets.open.bg_stroke.color = theme.border_color();
     ctx.set_visuals(visuals);
     ctx.style_mut_of(egui_theme, |style| {
+        style.spacing.scroll.dormant_handle_opacity = 0.2;
+        style.spacing.scroll.active_handle_opacity = 0.2;
+        style.spacing.scroll.interact_handle_opacity = 0.2;
         for font_id in style.text_styles.values_mut() {
             font_id.family = theme.system_font_family();
             font_id.size = theme.font_size;
